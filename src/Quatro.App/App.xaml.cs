4fea33c3f8ce01e471fe89b9810484bb4700b33f
ikeas68 <thales.letreste@gmail.com>
2025-10-20 11:31:53 +0200

    update for net48

--- src/Quatro.App/App.xaml.cs
+++ src/Quatro.App/App.xaml.cs
@@ -1,7 +1,17 @@
 using System.Windows;
 
-namespace Quatro;
-
-public partial class App : Application
+namespace Quatro
 {
+
+    public partial class App : Application
+    {
+    }
 }
+
+#if !NET5_0_OR_GREATER
+namespace System.Runtime.CompilerServices
+{
+    // Requis pour 'init' / records sur .NET Framework / netstandard
+    internal static class IsExternalInit { }
+}
+#endif
